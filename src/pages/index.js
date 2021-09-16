16301921abb7f38c9d04da235a9e9df58a4b86d1
muneebempglabs <muneeb.ahmed@empglabs.com>
2021-09-16 14:31:51 +0500

    added mapbox access token and styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,27 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import ReactMapGL from "react-map-gl"
+import { useState } from "react"
 
 export default function Home() {
+  const [viewport, setviewport] = useState({
+    latitude: 45.4211,
+    longitude: -75.6903,
+    width: "100vw",
+    height: "100vh",
+    zoom: 8
+  });
+
   return (
-<h1>test</h1>
+    <>
+    <ReactMapGL 
+    {...viewport} 
+    mapStyle='mapbox://styles/muneebempg/cktmqd6yrac3117p572pu7gis'
+    mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+    onViewportChange={viewport=>{
+      setviewport(viewport)
+    }}
+    >
+      markers here
+    </ReactMapGL>
+    </>
   )
 }
